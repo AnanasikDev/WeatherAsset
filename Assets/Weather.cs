e45324143dcd7d6d5a896612b598aedccf308bde
Олег Бровченков <oleg.brovchenkov@gmail.com>
2021-05-30 22:08:00 +0300

    Simple rain added

---
+++ Assets/Weather.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Weather : MonoBehaviour
+{
+    //[SerializeField] ParticleSystem rainSystem;
+    [SerializeField] Material mat;
+    [SerializeField] short density;
+    [SerializeField] float speed;
+    private void Update()
+    {
+        //rainSystem.emission.rateOverTime = new ParticleSystem.MinMaxCurve(density);
+
+        //if (!rainSystem.isPlaying) rainSystem.Play();
+
+        mat.SetTextureOffset(Shader.PropertyToID("_MainTex"), new Vector2(0, mat.mainTextureOffset.y + Time.deltaTime));
+    }
+}
